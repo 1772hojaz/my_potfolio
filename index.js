52f15107e97f6f6334049fb73643318c18219c08
Humphrey Nyahoja <97475818+1772hojaz@users.noreply.github.com>
2024-02-21 22:56:02 +0000

    added all files

---
+++ index.js
@@ -0,0 +1,54 @@
+/* toggle icon */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+/* scroll active link */
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector(`header nav a[href*="${id}"]`).classList.add('active');
+            });
+        }
+    });
+
+    /* sticky nav bar */
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+// Initialize ScrollReveal with options
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+// Use ScrollReveal on elements
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'bottom'});
+ScrollReveal().reveal('.home-contact h1, .about-img', {origin: 'left'});
+ScrollReveal().reveal('.home-contact p, .about-content', {origin: 'right'});
+
+/* typed js */
+const typed = new Typed('.multiple-text', {
+    strings: ['Full-Stack Developer', 'Python Developer', 'Tech-Enthusiast'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
